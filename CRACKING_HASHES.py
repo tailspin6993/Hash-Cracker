dbbd9c330f342b423fbeb6ea4a2f41473ca2ce7d
tailspin6993 <highdivide3@protonmail.com>
2023-12-29 17:42:37 -0500

    add argument parsing and algorithm lookup

--- CRACKING_HASHES.py
+++ CRACKING_HASHES.py
@@ -1,4 +1,19 @@
 import hashlib
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('digest', help='digest to attempt to crack')
+parser.add_argument('-a', '--algorithm', help='algorithm to use, default is SHA256')
+parser.add_argument('-s', '--salt', help='salt to use (if any)')
+
+args = parser.parse_args()
+
+ALGORITHMS = {
+    'md5': hashlib.md5,
+    'sha1': hashlib.sha1,
+    'sha256': hashlib.sha256,
+    'sha512': hashlib.sha512
+}
 
 def hash_file(file_path):
     with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
